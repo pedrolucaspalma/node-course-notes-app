7f5b37f3b7c9841058f2b3dedd6f6752826803a7
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-03 07:41:16 -0300

    finish class 17 (correct)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { argv } = require('yargs')
 const yargs = require ('yargs')
 
 yargs.version('1.1.0')
@@ -5,8 +6,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note!',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: function(){
-        console.log('Adding a new note!')
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -34,4 +48,6 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
+
+// console.log(yargs.argv)
