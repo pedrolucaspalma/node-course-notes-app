2fd7139ace2934fe84f778d0ba901a72c27fdc76
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-03 07:32:17 -0300

    finish class 17

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ yargs.version('1.1.0')
 
 yargs.command({
     command: 'add',
-    describe: 'Add a new note!'
+    describe: 'Add a new note!',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function(){
+        console.log('Listing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read note',
+    handler: function(){
+        console.log('Reading the note')
+    }
 })
 
 console.log(yargs.argv)
