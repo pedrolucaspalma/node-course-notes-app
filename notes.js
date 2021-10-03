e6fcfaee6dd03efbabbf879afe9b7fca0c83d45c
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-03 09:32:08 -0300

    finish class 20. install chalk

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function(){
     return 'Your notes...'
@@ -40,7 +41,19 @@ const loadNotes = function(){
 }
 
 const removeNote = function(title){
-    console.log(title)
+    const notes = loadNotes();
+
+    const notesToKeep = notes.filter(function (note){
+        return note.title !==title
+    })
+
+    if(notes.length > notesToKeep.length){
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else{
+        console.log(chalk.red.inverse('No note found!'))
+    }
+
 }
 
 module.exports = {
